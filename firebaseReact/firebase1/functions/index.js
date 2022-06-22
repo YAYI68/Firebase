ef0a4f78a24777ad490109d3a80638fefecd83fd
YAYI68 <yayiabiodun68@gmail.com>
2022-06-22 16:06:18 +0100

    commit

--- firebaseReact/firebase1/functions/index.js
+++ firebaseReact/firebase1/functions/index.js
@@ -1,9 +1,24 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+// exports.getAllPosts = functions.https.onRequest(async(request, response) => {
+     
+//       const posts = snapshots.doc.map(snapshot=>({uid:snapshot.uid,...snapshot.data()}))
+//       response.json({posts}) 
+//   });
+
+  exports.getAllPosts = functions.https.onRequest(async(request, response)=> {
+    const snapshots = await admin.firestore().collection('posts').get()
+    const posts = snapshots.doc.map(snapshot=>({uid:snapshot.uid,...snapshot.data()}))
+    response.json({posts}) 
+  });
